7217fe00e141782424015e50b0616ad26bbe29d0
Kenz <kenz@yahoo.com>
2025-11-06 09:12:33 +0530

    SubSetOfString - Invalid number of parts. Exception handled

--- src/Assn_2025_11_05/StringIntoNParts.java
+++ src/Assn_2025_11_05/StringIntoNParts.java
@@ -6,12 +6,20 @@ import java.util.Scanner;
 public class StringIntoNParts {
     public static void main(String[] args) {
         System.out.println("*14. Java Program to Divide a String into N Parts");
-        String str = "A aB bC c1";
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the String data");
+        String str =sc.nextLine();//Example A aB bC c1
+
         System.out.println("How many parts you want the String to be broken into:");
         int parts = sc.nextInt();
 
         int totalLength = str.length();
+        if (parts <= 0 || parts > totalLength) {
+            System.out.printf("Invalid number of parts. Please enter parts less than %d",totalLength);
+            return;
+        }
+
         int equalPartSize = totalLength / parts;
         int start = 0, end = equalPartSize;
 
