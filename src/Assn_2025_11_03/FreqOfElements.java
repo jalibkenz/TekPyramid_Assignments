ddec9aab9c46a86c133e1b1a85f9f13335f1fbca
Kenz <kenz@yahoo.com>
2025-11-17 08:40:35 +0530

    Freq without Collections

--- src/Assn_2025_11_03/FreqOfElements.java
+++ src/Assn_2025_11_03/FreqOfElements.java
@@ -18,5 +18,24 @@ public class FreqOfElements {
         for(Map.Entry<String, Integer> x: wordList.entrySet()){//Using Entry to fetch entry in entries (key value pairs)
             System.out.printf("%10s | appears | %s\n",x.getKey(),x.getValue());
         }
+        //Another way
+        CharFrequency.main();
+    }
+
+
+}
+
+
+//Frequency of elements from String Literal
+class CharFrequency {
+    public static void main() {
+        String str = "hello world";
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            if (c != ' ')  // ignore spaces
+                freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        System.out.println("Character frequency: " + freq);
     }
 }
